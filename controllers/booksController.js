4073f00975915f769bf58b861e378221f9b06606
annahuhtanen <anna.huhtanen@edu.turkuamk.fi>
2021-12-02 13:38:36 +0200

    books and movie controller

---
+++ controllers/booksController.js
@@ -0,0 +1,115 @@
+"use strict";
+
+const Book = require("../models/book"),
+  getBookParams = body => {
+    return {
+      title: body.title,
+      author: body.author,
+      genre: body.genre,
+      publishYear: body.publishYear,
+      description: body.description
+    };
+  };
+
+module.exports = {
+  index: (req, res, next) => {
+    Book.find()
+      .then(books => {
+        res.locals.books = books;
+        next();
+      })
+      .catch(error => {
+        console.log(`Error fetching books: ${error.message}`);
+        next(error);
+      });
+  },
+  indexView: (req, res) => {
+    res.render("books/index");
+  },
+
+  new: (req, res) => {
+    res.render("books/new");
+  },
+
+  create: (req, res, next) => {
+    let bookParams = getBookParams(req.body);
+    Book.create(bookParams)
+      .then(book => {
+        res.locals.redirect = "/books";
+        res.locals.book = book;
+        next();
+      })
+      .catch(error => {
+        console.log(`Error saving book: ${error.message}`);
+        next(error);
+      });
+  },
+
+  redirectView: (req, res, next) => {
+    let redirectPath = res.locals.redirect;
+    if (redirectPath !== undefined) res.redirect(redirectPath);
+    else next();
+  },
+
+  show: (req, res, next) => {
+    let bookId = req.params.id;
+    Book.findById(bookId)
+      .then(book => {
+        res.locals.book = book;
+        next();
+      })
+      .catch(error => {
+        console.log(`Error fetching book by ID: ${error.message}`);
+        next(error);
+      });
+  },
+
+  showView: (req, res) => {
+    res.render("books/show");
+  },
+
+  edit: (req, res, next) => {
+    let bookId = req.params.id;
+    Book.findById(bookId)
+      .then(book => {
+        res.render("books/edit", {
+          book: book
+        });
+      })
+      .catch(error => {
+        console.log(`Error fetching book by ID: ${error.message}`);
+        next(error);
+      });
+  },
+
+  update: (req, res, next) => {
+    let bookId = req.params.id,
+      bookParams = getBookParams(req.body);
+
+    Book.findByIdAndUpdate(bookId, {
+      $set: bookParams
+    })
+      .then(book => {
+        res.locals.redirect = `/books/${bookId}`;
+        res.locals.book = book;
+        next();
+      })
+      .catch(error => {
+        console.log(`Error updating book by ID: ${error.message}`);
+        next(error);
+      });
+  },
+
+  delete: (req, res, next) => {
+    let bookId = req.params.id;
+    Book.findByIdAndRemove(bookId)
+      .then(() => {
+        res.locals.redirect = "/books";
+        next();
+      })
+      .catch(error => {
+        console.log(`Error deleting book by ID: ${error.message}`);
+        next();
+      });
+  }
+};
---
+++ controllers/moviesController.js
@@ -0,0 +1,115 @@
+"use strict";
+
+const Movie = require("../models/movie"),
+  getMovieParams = body => {
+    return {
+      title: body.title,
+      director: body.director,
+      genre: body.genre,
+      releaseYear: body.releaseYear,
+      description: body.description
+    };
+  };
+
+module.exports = {
+  index: (req, res, next) => {
+    Movie.find()
+      .then(movies => {
+        res.locals.movies = movies;
+        next();
+      })
+      .catch(error => {
+        console.log(`Error fetching movies: ${error.message}`);
+        next(error);
+      });
+  },
+  indexView: (req, res) => {
+    res.render("movies/index");
+  },
+
+  new: (req, res) => {
+    res.render("movies/new");
+  },
+
+  create: (req, res, next) => {
+    let movieParams = getMovieParams(req.body);
+    Movie.create(movieParams)
+      .then(movie => {
+        res.locals.redirect = "/movies";
+        res.locals.book = movie;
+        next();
+      })
+      .catch(error => {
+        console.log(`Error saving movie: ${error.message}`);
+        next(error);
+      });
+  },
+
+  redirectView: (req, res, next) => {
+    let redirectPath = res.locals.redirect;
+    if (redirectPath !== undefined) res.redirect(redirectPath);
+    else next();
+  },
+
+  show: (req, res, next) => {
+    let movieId = req.params.id;
+    Movie.findById(movieId)
+      .then(movie => {
+        res.locals.movie = movie;
+        next();
+      })
+      .catch(error => {
+        console.log(`Error fetching movie by ID: ${error.message}`);
+        next(error);
+      });
+  },
+
+  showView: (req, res) => {
+    res.render("movies/show");
+  },
+
+  edit: (req, res, next) => {
+    let movieId = req.params.id;
+    Movie.findById(movieId)
+      .then(movie => {
+        res.render("movies/edit", {
+          movie: movie
+        });
+      })
+      .catch(error => {
+        console.log(`Error fetching movie by ID: ${error.message}`);
+        next(error);
+      });
+  },
+
+  update: (req, res, next) => {
+    let movieId = req.params.id,
+      movieParams = getMovieParams(req.body);
+
+    Movie.findByIdAndUpdate(movieId, {
+      $set: movieParams
+    })
+      .then(movie => {
+        res.locals.redirect = `/movies/${movieId}`;
+        res.locals.movie = movie;
+        next();
+      })
+      .catch(error => {
+        console.log(`Error updating movie by ID: ${error.message}`);
+        next(error);
+      });
+  },
+
+  delete: (req, res, next) => {
+    let movieId = req.params.id;
+    Movie.findByIdAndRemove(movieId)
+      .then(() => {
+        res.locals.redirect = "/movies";
+        next();
+      })
+      .catch(error => {
+        console.log(`Error deleting movie by ID: ${error.message}`);
+        next();
+      });
+  }
+};

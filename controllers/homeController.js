44f50a61e71454995ad2b12e51585b467b4f989b
Niakku <niakku.nieminen@gmail.com>
2021-12-06 13:31:38 +0200

    added logRequestPaths to homeController

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,5 +3,9 @@
 module.exports = {
   index: (req, res) => {
     res.render("index");
+  },
+  logRequestPaths: (req, res, next) => {
+    console.log(`request made to: ${req.url}`);
+    next();
   }
 };
